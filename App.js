4bd654a8b5e47d9620d0a60c7331f287299e3a9a
Sampurna321 <83407901+Sampurna321@users.noreply.github.com>
2024-01-13 22:21:50 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading2 = (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
